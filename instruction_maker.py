25635ed3ff76632de768ba1cae43b23f8fcddce9
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-06-16 20:43:19 +0200

    Update instruction_maker.py
    # To do : test traductor bin to mips32

--- instruction_maker.py
+++ instruction_maker.py
@@ -95,27 +95,27 @@ def translate_bin_to_mips32(bin_instruction: str = '0'*32, test=False) -> (str):
     def find_mips_from_bin(bin_word, dictionary: dict):
         for key, value in dictionary.items():
             if value == bin_word: return key
+        else: return 'Unfound'
     
-    opcode = find_mips_from_bin(bin_instruction[:7], MIPS_INSTRUCTION_DICT_BIN_OP)
+    opcode = find_mips_from_bin(bin_instruction[:6], MIPS_INSTRUCTION_DICT_BIN_OP)
     if test: 
         print('In function translate_bin_to_mips32')
         print(opcode)
     
     if opcode in MIPS_R_TYPE:
-        rs = find_mips_from_bin(bin_instruction[7:12], MIPS_REGISTER_DICT_BIN)
+        rs = find_mips_from_bin(bin_instruction[6:11], MIPS_REGISTER_DICT_BIN)
         if test: print(rs)
         
-        rt = find_mips_from_bin(bin_instruction[12:17], MIPS_REGISTER_DICT_BIN)
+        rt = find_mips_from_bin(bin_instruction[11:16], MIPS_REGISTER_DICT_BIN)
         if test: print(rt)
         
-        rd = find_mips_from_bin(bin_instruction[17:22], MIPS_REGISTER_DICT_BIN)
+        rd = find_mips_from_bin(bin_instruction[16:21], MIPS_REGISTER_DICT_BIN)
         if test: print(rd)
         
-        shamt = '0'*5
-        func = find_mips_from_bin(bin_instruction[25:], MIPS_INSTRUCTION_DICT_BIN_FUNCT)
+        func = find_mips_from_bin(bin_instruction[27:], MIPS_INSTRUCTION_DICT_BIN_FUNCT)
         if test: print(func)
         
-        mips_code = func + rd + rs + rt
+        mips_code = f"{func} {rd}, {rs}, {rt}"
     
     elif opcode in MIPS_I_TYPE:
         rs = find_mips_from_bin(bin_instruction[7:12], MIPS_REGISTER_DICT_BIN)
@@ -134,16 +134,16 @@ def translate_bin_to_mips32(bin_instruction: str = '0'*32, test=False) -> (str):
     elif opcode in MIPS_J_TYPE:        
         address = int(bin_instruction[7:], 2)
         if test: print(address)
-        mips_code = opcode + address
+        mips_code = f"{opcode} {address}"
     
     else: 
-        mips_code = ValueError(f"The binary instruction did not find mips instruction wich correspond to {bin_instruction}")
+        raise ValueError(f"The binary instruction did not find mips instruction wich correspond to {bin_instruction}")
 
     return mips_code
         
 
-def translate_mips32_to_bin(mips_instruction : str = 'add $s1, $s1, $s2', test=False):
-    mips_instruction_list = [a.strip(',') for a in mips_instruction.split(' ' )]
+def translate_mips32_to_bin(mips_instruction: str = 'add $s1, $s1, $s2', test=False):
+    mips_instruction_list = [a.strip(',') for a in mips_instruction.split(' ')] # list of word in mips ex: ['add', '$s1', '$t1', $t2']
     if test:
         print('In function translate_mips32_to_bin')
         print(mips_instruction_list)
@@ -201,56 +201,52 @@ def translate_mips32_to_bin(mips_instruction : str = 'add $s1, $s1, $s2', test=F
     return bin_instruction
 
 def create_J_type_bin(test=False) -> (str):
-    instruction = random.choice(MIPS_J_TYPE)
-    opcode = MIPS_INSTRUCTION_DICT_BIN_OP[instruction]
+    opcode = random.choice(MIPS_J_TYPE)
     if test:
         print("In function create_J_type_bin")
         print(opcode)
         
-    address = set_lenght(bin(random.randint(0,67108863))[2:], 26)
+    address = random.randint(0,67108863)
     if test: print(address)
     
-    return opcode + address
+    return  f"{opcode} {address}"
 
 def create_I_type_bin(test=False) -> (str):
-    instruction = random.choice(MIPS_I_TYPE)
-    opcode = MIPS_INSTRUCTION_DICT_BIN_OP[instruction]
+    opcode = random.choice(MIPS_I_TYPE)
     if test:
         print("In function create_I_type_bin")
         print(opcode)
     
-    rs = MIPS_REGISTER_DICT_BIN[random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))]
+    rs = random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))
     if test: print(rs)
     
-    rt = MIPS_REGISTER_DICT_BIN[random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))]
+    rt = random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))
     if test: print(rt)
     
-    address = set_lenght( bin(random.randint(0, 65535))[2:], 16)
+    address = random.randint(0, 65535)
     if test: print(address)
     
-    return opcode + rs + rt + address
+    if opcode in ('lw', 'sw'): mips_code = f"{opcode} {rt}, {address}({rs})"
+    else: mips_code = f"{opcode} {rs}, {rt}, {address}"
+    
+    return mips_code
 
 def create_R_Type_bin(test=False) -> (str):
-    opcode = '0' * 6
-    if test:
-        print("In function create_R_Type_bin")
-        print(opcode)
+    if test: print("In function create_R_Type_bin")
         
-    rs = MIPS_REGISTER_DICT_BIN[random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))]
+    rs = random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))
     if test: print(rs)
         
-    rt = MIPS_REGISTER_DICT_BIN[random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))]
+    rt = random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))
     if test: print(rt)
         
-    rd = MIPS_REGISTER_DICT_BIN[random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))]
+    rd =random.choice(list(MIPS_REGISTER_DICT_BIN.keys()))
     if test: print(rd)
-        
-    shamt = '0'*5
     
-    funct = MIPS_INSTRUCTION_DICT_BIN_FUNCT[random.choice(list(MIPS_INSTRUCTION_DICT_BIN_FUNCT.keys()))]
-    if test: print(funct)
+    func = random.choice(list(MIPS_INSTRUCTION_DICT_BIN_FUNCT.keys()))
+    if test: print(func)
         
-    return opcode + rs + rt + rd + shamt + funct
+    return f"{func} {rd}, {rs}, {rt}"
 
 def random_instruction(test=False) -> (str):
     """generate one random instrcution in mips32 writen in binary
@@ -260,19 +256,19 @@ def random_instruction(test=False) -> (str):
         default_instruction : instruction in mips 32 for correction (str)
     """
     
-    bin_instruction = None
+    mips_instruction = None
     instruction_type: Callable = random.choice((create_R_Type_bin, create_I_type_bin, create_J_type_bin))
     
     if test:
         print("In function random_instruction")
-        print('bin_instruction : ', bin_instruction)
+        print('bin_instruction : ', mips_instruction)
         print('instruction_type : ', instruction_type)
     
-    bin_instruction = instruction_type(test=test)
+    mips_instruction = instruction_type(test=test)
     
-    if test: print('bin_instruction after call of function : ', bin_instruction)
+    if test: print('bin_instruction after call of function : ', mips_instruction)
     
-    mips_instruction = translate_bin_to_mips32(bin_instruction, test=test)
+    bin_instruction = translate_mips32_to_bin(mips_instruction, test=test)
     
     if test: print('default_instrcution : ', mips_instruction)
         
@@ -282,6 +278,7 @@ def random_instruction(test=False) -> (str):
 def __main__(test=False):
     import os
     import time
+    
     def clear():
         command = 'clear'
         if os.name == 'nt': command = 'cls'
@@ -299,7 +296,7 @@ def __main__(test=False):
         if user_anwer == mips_instruction: print ("\nYou have performed admirably")
         else: print("\nYou will be more lucki next time, but you are on the right way")
         
-        time.sleep(30)
+        time.sleep(3)
         
 
 if __name__ == '__main__':
