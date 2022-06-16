0eb3a65dd687c72d7aba59e89c9d460ea9135a78
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-06-16 20:48:42 +0200

    Update test_instruction_maker.py
    crarification of code

--- test_instruction_maker.py
+++ test_instruction_maker.py
@@ -1,49 +1,19 @@
 import unittest
 
 from typing import Callable
+
+# module to test
 import instruction_maker as im
 
 class TestInstructionMaker(unittest.TestCase):
-    def test_translate_bin_to_mips32(self, mips_expected, binary):
     
-        returned = im.translate_bin_to_mips32(binary)
+    def test_function(self, func: Callable = im.random_instruction, arg: str = None, expected: str = None, test=True):
 
-        self.assertEqual(mips_expected, returned)
+        returned = func(test) if arg != None else func(arg, test=test)
         
-    def test_translate_mips32_to_bin(self, mips, binary_expected):
-    
-        returned = im.translate_mips32_to_bin(mips)
-
-        self.assertEqual(binary_expected, returned)
-        
-    
+        self.assertEqual(returned, expected)
     
     def setUp(self):
-        def test_function(func: Callable = im.random_instruction, arg=None, expected=None, test=True):
-            print("In function : ", func)
-            try:
-                if arg != None:
-                    r =  func(test=test)
-                    print(r)
-                    return None
-                r = func(arg, test=test)
-                if r != expected: print(f"expected : {expected} \nreturned : {r}")
-            except Exception as e:
-                print("Error : ", e)
-        
-        test_function(im.random_instruction)
-        print()
-        
-        test_function(im.create_R_Type_bin)
-        print()
-        
-        test_function(im.create_J_type_bin)
-        print()
-        
-        test_function(im.create_I_type_bin)
-        print()   
-
-        # test_function(__main__)
         data_to_test = [
             {
                 'mips' : "slt $t5, $t6, $a8",
@@ -82,9 +52,16 @@ class TestInstructionMaker(unittest.TestCase):
                 'bin' : '00001100000000000000100000101110',
             },
         ]
+        self.test_function(self, im.random_instruction)
+        
+        self.test_function(self, im.create_R_Type_bin)
+        
+        self.test_function(self, im.create_J_type_bin)
+        
+        self.test_function(self, im.create_I_type_bin)
         
         for dic in data_to_test:
-            self.test_translate_bin_to_mips32(dic['mips'], dic['bin'])
+            self.test_function(self, im.translate_bin_to_mips32, dic['mips'], dic['bin'])
+            self.test_function(self, im.translate_mips32_to_bin, dic['bin'], dic['mips'])
             
-        for dic in data_to_test:
-            self.test_translate_mips32_to_bin(dic['mips'], dic['bin'])
+        self.test_function(self, im.__main__)
